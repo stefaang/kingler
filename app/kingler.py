5f1fb5f2ff1f6015027204561da31ff280eb233b
Stefaan Ghysels <stefaang@gmail.com>
2016-09-20 18:02:02 +0200

    disable mongo stuff for now

--- app/kingler.py
+++ app/kingler.py
@@ -1,17 +1,17 @@
 from flask import Flask, redirect, request, render_template
-from flask_mongoengine import MongoEngine
+#from flask_mongoengine import MongoEngine
 #from flask_runner import Runner
 
-db = MongoEngine()
+#db = MongoEngine()
 
 app = Flask(__name__)
-app.config['MONGODB_SETTTINGS'] = {
-    'db':'kingler',
+#app.config['MONGODB_SETTTINGS'] = {
+#    'db':'kingler',
 #    'host': xx
 #    'port':27017
 }
-app.config.from_pyfile('kingler.cfg')
-db.init_app(app)
+#app.config.from_pyfile('kingler.cfg')
+#db.init_app(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def show_main():
