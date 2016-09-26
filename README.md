6d33b0800c26b9e995539cf4a86837c8674358bc
Stefaan Ghysels <stefaang@gmail.com>
2016-09-27 00:26:01 +0200

    messing with shapely

---
+++ README.md
@@ -0,0 +1,6 @@
+Build a geospatial web app with Flask and deploy it on Heroku.
+
+
+Requirements
+- Flask
+- PostGreSQL with PostGIS and GeoAlchemy2
--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import os
 import json
-from flask import Flask, render_template, request
+from flask import Flask, session, redirect, url_for, escape, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 from geoalchemy2 import func
-from shapely import wkb
+from geoalchemy2.shape import to_shape
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
@@ -13,19 +13,51 @@ db = SQLAlchemy(app)
 from models import *
 
 
+
+
+
+
 @app.route('/')
-def hello():
-    return "Hello World"
+def index():
+    if 'username' in session:
+        return 'Logged in as %s' % escape(session['username'])
+    return 'You are not logged in'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        safename = escape(session['username'])
+        session['racer'] = db.session.query(Racer).filter_by(name=safename.first()
+        if not session['racer']:
+            db.session.add(Racer(safename, 'POINT(0 0)'))
+            db.session.commit()
+        return redirect(url_for('index'))
+    return '''
+        <form action="" method="post">
+            <p><input type=text name=username>
+            <p><input type=submit value=Login>
+        </form>
+    '''
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
 
 @app.route('/map')
 def show_map():
-    # get points from db
-    rquery = db.session.query(Racer)
-    racers = []  
-    for r in rquery:
-        pos = wkb.loads(bytes(r.pos.data))
-        racers.append({'name':r.name, 'x':pos.x, 'y': pos.y})
-    return render_template('map.html', racers=racers)
+    if 'username' in session:
+        # get points from db
+        rquery = db.session.query(Racer)
+        racers = []
+        for r in rquery:
+            pos = to_shape(r.pos)
+            racers.append({'name':r.name, 'x':pos.x, 'y': pos.y})
+        return render_template('map.html', racers=racers, user=escape(session['username']))
+    else:
+        return 'You are not logged in'
 
 @app.route('/moveit', methods=['GET','POST'])
 def move_marker():
@@ -36,7 +68,7 @@ def move_marker():
         s = db.session
         try:
             #r = Racer('yolo', 'POINT(%s %s)' % (lat, lng))
-            r = s.query(Racer).filter_by(name=name).first()
+            r = s.query(Racer.name, ).filter_by(name=name).first()
             r.pos = 'POINT(%s %s)' % (lat, lng)
             s.commit()
             return "OKOK"
